97a37f3e68fdbb021998831060c22cad1b22ee94
Aizak1 <73393039+Aizak1@users.noreply.github.com>
2021-04-01 16:44:18 +0300

    RookFix

--- Chess/Assets/ChessModels/Script/Figure.cs
+++ Chess/Assets/ChessModels/Script/Figure.cs
@@ -13,6 +13,29 @@ public abstract class Figure : MonoBehaviour
         new Vector2Int(-1, -1), new Vector2Int(-1, 1)};
 
     public abstract bool IsAbleToMove(Figure figureToCapture,Vector2Int gridPoint);
+
+    protected bool DirectionalFigureIsAbleToMove(Figure figureToCapture, Vector2Int gridPoint,Vector2Int[] allPossibleDirections)
+    {
+        Vector2Int[] figuresPositions = FindObjectsOfType<Figure>().Select(figure => figure.Data.position).ToArray();
+        if (figureToCapture != null && figureToCapture.Data.isWhite == Data.isWhite)
+            return false;
+        var initialCoordinate = Data.position;
+        var delta = (Vector2)(gridPoint - initialCoordinate);
+        var directionalStep = new Vector2Int((int)delta.normalized.x, (int)delta.normalized.y);
+        if (allPossibleDirections.Contains(directionalStep))
+        {
+            initialCoordinate += directionalStep;
+            while (initialCoordinate != gridPoint)
+            {
+                if (figuresPositions.Contains(initialCoordinate))
+                    return false;
+                initialCoordinate += directionalStep;
+            }
+            return true;
+        }
+
+        return false;
+    }
     
     protected Vector2Int CalculateDelta(Vector2Int initialPosition,Vector2Int finalPosition)
     {
--- Chess/Assets/ChessModels/Script/Figures/Rook.cs
+++ Chess/Assets/ChessModels/Script/Figures/Rook.cs
@@ -7,24 +7,6 @@ public class Rook : Figure
 {
     public override bool IsAbleToMove(Figure figureToCapture, Vector2Int gridPoint)
     {
-        Vector2Int[] figuresPositions = FindObjectsOfType<Figure>().Select(figure => figure.Data.position).ToArray();
-        if (figureToCapture != null && figureToCapture.Data.isWhite == Data.isWhite)
-            return false;
-        var initialCoordinate = Data.position;
-        var delta = (Vector2)(gridPoint - initialCoordinate);
-        var directionalStep = new Vector2Int((int)delta.normalized.x, (int)delta.normalized.y);
-        if (rookDirections.Contains(directionalStep))
-        {
-            initialCoordinate += directionalStep;
-            while (initialCoordinate != gridPoint+directionalStep)
-            {
-                if (figuresPositions.Contains(initialCoordinate))
-                    return false;
-                initialCoordinate += directionalStep;
-            }
-            return true;
-        }
-
-        return false;
+        return DirectionalFigureIsAbleToMove(figureToCapture, gridPoint, rookDirections);
     }
 }
