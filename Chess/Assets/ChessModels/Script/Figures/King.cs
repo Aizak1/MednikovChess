8671a4f04ba7f9b687ac0039611825dd508b1ed2
Aizak1 <73393039+Aizak1@users.noreply.github.com>
2021-04-01 15:32:56 +0300

    King

--- Chess/Assets/ChessModels/Script/Figures/King.cs
+++ Chess/Assets/ChessModels/Script/Figures/King.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class King : Figure
 {
-    public override bool IsAbleToMove(Figure figureToCapture, Vector2Int gridPoint)
+    public override bool IsAbleToMove(Figure figureToCapture, Vector2Int finalPosition)
     {
-        return true;
+        if (figureToCapture != null && figureToCapture.Data.isWhite == Data.isWhite)
+            return false;
+        List<Vector2Int> directions = new List<Vector2Int>(bishopDirections);
+        directions.AddRange(rookDirections);
+        return directions.Contains(CalculateDelta(Data.position, finalPosition));
+        
+
     }
 }
